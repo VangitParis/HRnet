49b60d67d0f1b6f46888db20d0863a5a73b071ea
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-12-19 08:57:49 +0100

    corrected errors

--- src/components/Dropdown/dropdown.jsx
+++ src/components/Dropdown/dropdown.jsx
@@ -7,42 +7,46 @@ export default function Dropdown({ value, onChange, id }) {
     const [selectedOption, setSelectedOption] = useState(value || null);
     
     
-  const [inputValue, setInputValue] = useState("");
+    const [inputValue, setInputValue] = useState("");
 
-  useEffect(() => {
-    // Affiche le premier élément de la liste au montage du composant
-    const firstOption =
-      id === "state" ? states[0] : id === "department" ? departments[0] : null;
+    useEffect(() => {
+        // Affiche le premier élément de la liste au montage du composant
+        const firstOption =
+            id === "state" ? states[0] : id === "department" ? departments[0] : null;
 
-    setSelectedOption({
-      value: firstOption ? firstOption.value : null,
-      label: firstOption ? firstOption.name : null,
-    });
-  }, [id]);
+        setSelectedOption({
+            value: firstOption ? firstOption.value : null,
+            label: firstOption ? firstOption.name : null,
+        });
+    }, [id]);
 
-  const options =
-    id === "state"
-      ? states.map((state) => ({
-          value: state.abbreviation,
-          label: state.name,
-        }))
-      : departments.map((department) => ({
-          value: department.value,
-          label: department.name,
-        }));
+    const options =
+        id === "state"
+            ? states.map((state) => ({
+                value: state.abbreviation,
+                label: state.name,
+            }))
+            : departments.map((department) => ({
+                value: department.value,
+                label: department.name,
+            }));
 
-  const handleChange = (selectedOption) => {
-    setSelectedOption(selectedOption);
-    setInputValue(selectedOption ? selectedOption.label : "");
-    onChange(selectedOption);
-  };
+    const handleChange = (selectedOption) => {
+        setSelectedOption(selectedOption);
+        setInputValue(selectedOption ? selectedOption.label : "");
+        onChange(selectedOption);
+    };
 
-  return (
-    <Select
-      onChange={handleChange}
-      options={options}
-      value={selectedOption}
-      hideSelectedOptions={false}
-    />
-  );
+    return (
+        <Select
+            autoFocus
+            onChange={handleChange}
+            options={options}
+            value={selectedOption || inputValue}
+            hideSelectedOptions={false}
+
+        />
+    
+    );
 }
+
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,18 +4,12 @@ import "../../styles/sass/pages/_home.scss";
 import Form from "../../components/Form/form";
 
 export default function Home() {
-
-
-  
-
   return (
     <main className="container-fluid gradient-background mt-0 d-flex flex-column justify-content">
       <h1 className="text-center">Create Employee</h1>
       <div className="d-flex align-items-center mt-5 flex-lg-row flex-column">
-       <Form/>
+        <Form />
       </div>
-
-      
     </main>
   );
 }
