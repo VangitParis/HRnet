cf90c66f66ebf61d4bf1e5c0bc07da3f261314ae
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-12-20 12:42:12 +0100

    Updates

--- src/utils/departments.js
+++
@@ -1,8 +0,0 @@
-const departments = [
-  { value: "Marketing", name: "Marketing" },
-  { value: "Engineering", name: "Engineering" },
-  { value: "Human Resources", name: "Human Resources" },
-  { value: "Legal", name: "Legal" },
-];
-
-export default departments;
